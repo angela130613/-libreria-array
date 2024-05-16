6ea31db7e01fc86d598514ec33fa6d1ed5ab2fa6
Angela Maria Yepes Ramirez <andres2345v@gmail.com>
2024-05-16 11:30:30 -0500

    segundo libro

--- index.js
+++ index.js
@@ -19,5 +19,27 @@ let libro = [
             grueso: "3.2 cm",
         },
         peso: "544 gr",
+    },
+
+    {
+        titulo: "Culpa tuya",
+        autor: "Mercedes Ron",
+        genero: "Romance adolescente",
+        idioma: "Español España",
+        precio: "$ 65.000 col.",
+        formato: "Tapa blanda bolsillo",
+        isbn: "9788413142029",
+        descripcion: "Cuando Noah se enamoró de Nick sabía que su relación no iba a ser fácil: son polos opuestos, fuego y electricidad, y cuando están juntos saltan chispas... en todos los sentidos. Hasta ahora, la pasión ha sido más fuerte que el orgullo, pero la diferencia de edad, la universidad, las fiestas, sus padres y los fantasmas que acechan a los dos les ponen a prueba una y otra vez, como una bomba de relojería que amenaza con hacerles estallar. ¿Está Noah realmente preparada para enfrentarse a sus miedos y volver a confiar en alguien? ¿Podrá Nicholas dejar atrás su pasado y abrir el corazón a una sola persona?",
+        estado: "Nuevo",
+        ubicacion: "Panamericana",
+        fecha_publicacion: "21 de septiembre de 2017",
+        editorial: "MONTENA",
+        paginas: "448",
+        dimensiones: {
+            alto: "19.1 cm",
+            ancho: "13 cm",
+            grueso: "2.8 cm",
+        },
+        peso: "325 gr",
     }
 ]
