afa315aa07a258e1cff5d6668e10f811262c243e
Angela Maria Yepes Ramirez <andres2345v@gmail.com>
2024-05-16 11:15:22 -0500

    setup inicial proyecto practica array

---
+++ index.js
@@ -0,0 +1,23 @@
+let libro = [
+    {
+        titulo: "Culpa Mia",
+        autor: "Mercedes Ron",
+        genero: "Romance adolescente",
+        idioma: "Español España",
+        precio: "$ 56.000 col.",
+        formato: "Tapa blanda",
+        isbn: "9788490438664",
+        descripcion: "Nicholas Leister ha sido creado para amargarme la vida. Alto, ojos azules, pelo negro como la noche... Suena genial ¿verdad? Pues no tanto cuando te enteras de que va a ser tu hermanastro y además representa todo de lo que has estado huyendo desde que tienes uso de razón.Peligro fue lo primero que me vino a la cabeza cuando lo conocí y descubrí que mantiene una doble vida oculta de su padre multimillonario. ¿Cómo termine cayendo en sus redes? Fácil: con esos ojos es capaz de poner tu mundo patas arriba.",
+        estado: "Nuevo",
+        ubicacion: "Panamericana",
+        fecha_publicacion: "08/06/2017",
+        editorial: "MONTENA",
+        paginas: "448",
+        dimensiones: {
+            alto: "21.5 cm",
+            ancho: "15.2 cm",
+            grueso: "3.2 cm",
+        },
+        peso: "544 gr",
+    }
+]
