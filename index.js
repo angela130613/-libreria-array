e9f1ad574180432f76eb5aa55a1fc59e124e6cc9
Angela Maria Yepes Ramirez <andres2345v@gmail.com>
2024-05-16 15:13:18 -0500

    Tercer Libro

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ let libro = [
         descripcion: "Cuando Noah se enamoró de Nick sabía que su relación no iba a ser fácil: son polos opuestos, fuego y electricidad, y cuando están juntos saltan chispas... en todos los sentidos. Hasta ahora, la pasión ha sido más fuerte que el orgullo, pero la diferencia de edad, la universidad, las fiestas, sus padres y los fantasmas que acechan a los dos les ponen a prueba una y otra vez, como una bomba de relojería que amenaza con hacerles estallar. ¿Está Noah realmente preparada para enfrentarse a sus miedos y volver a confiar en alguien? ¿Podrá Nicholas dejar atrás su pasado y abrir el corazón a una sola persona?",
         estado: "Nuevo",
         ubicacion: "Panamericana",
-        fecha_publicacion: "21 de septiembre de 2017",
+        fecha_publicacion: "21/09/2017",
         editorial: "B de Bolsillo",
         paginas: "448",
         dimensiones: {
@@ -42,6 +42,29 @@ let libro = [
         },
         peso: "325 gr",
     },
+
+    {
+        titulo: "Culpa nuestra",
+        autor: "Mercedes Ron",
+        genero: "Romance adolescente",
+        idioma: "Español España",
+        precio: "$ 65.000 col.",
+        formato: "Tapa blanda",
+        isbn: "9788490438893",
+        descripcion: "La relación de Nick y Noah está pasando por su peor momento, y parece que nada podrá volver a ser como antes... Van a tener que pasar por muchas cosas para finalmente comprender si de verdad están hechos el uno para el otro o si por lo contrario, estar separados es lo que de verdad les conviene. Pero ¿acaso se puede olvidar un amor tan fuerte? ¿Cómo pueden borrarse los recuerdos tatuados en el corazón? El amor no siempre es suficiente y el perdón a veces no basta para solucionar las cosas. ¿Serán capaces de dejar atrás el pasado y volver a empezar?",
+        estado: "Nuevo",
+        ubicacion: "Panamericana",
+        fecha_publicacion: "01/02/2018",
+        editorial: "Montena",
+        paginas: "448",
+        dimensiones: {
+            alto: "21.5 cm",
+            ancho: "15.4 cm",
+            grueso: "3.2 cm",
+        },
+        peso: "540 gr",
+    },
+
 
     
 ]
