d6b0aa508738ca2d3fcbf8bbd7ef68240c404dde
Angela Maria Yepes Ramirez <andres2345v@gmail.com>
2024-05-23 09:40:22 -0500

    menu edition

--- index.js
+++ index.js
@@ -504,10 +504,11 @@ function mostrarLibros() {
 let msj
 msj = "opciones de edición para los libros\n\n";
 msj += "1. Agregar libro\n";
-msj += "2. Eliminar el último libro agregado\n"
-msj += "3. Mostrar lista de libros\n\n"
+msj += "2. Eliminar un libro por su nombre"
+msj += "3. Eliminar el último libro agregado\n"
+msj += "4. Mostrar lista de libros\n\n"
 msj += "ingrese el código de la acción que quiere realizar siendo entre 1 y 3"
-codigo = parseInt(prompt(mjs));
+ let codigo = parseInt(prompt(msj));
 switch (código) {
   case 1:
     agregarLibro();
